5fc95581785e547c96d130dba5cfd35096f05c0d
WKHAllen <wkhallen@gmail.com>
2022-01-19 17:39:17 -0500

    Support building both frontend and backend

--- build.bat
+++ build.bat
@@ -1,3 +1,20 @@
 @echo off
 
-go build -o bin
+rem Check if backend only
+if "%1"=="backend" goto :backend
+
+rem Build frontend
+:frontend
+cd webui
+cmd /c npm run build
+cd ..
+
+rem Check if frontend only
+if "%1"=="frontend" goto :eof
+
+rem Build backend
+:backend
+cmd /c go build -o bin
+
+rem End of file
+:eof
--- build.sh
+++ build.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-go build -o bin
+# Build frontend
+build_frontend()
+{
+  cd webui
+  npm run build
+  cd ..
+}
+
+# Build backend
+build_backend()
+{
+  go build -o bin
+}
+
+main()
+{
+  if [[ $1 == 'frontend' ]]; then
+    build_frontend
+  elif [[ $1 == 'backend' ]]; then
+    build_backend
+  else
+    build_frontend
+    build_backend
+  fi
+}
+
+main "$@"; exit
