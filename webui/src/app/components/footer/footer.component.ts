2763706d9eb6fa88105d6d4519c16e72e50942d8
WKHAllen <wkhallen@gmail.com>
2022-01-29 10:51:33 -0500

    Create frontend layout

---
+++ webui/src/app/components/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eno-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.less'],
+})
+export class FooterComponent {}
