3c8dd31a07ebadf39c97535f2cbaa23fe5f3d3c4
WKHAllen <wkhallen@gmail.com>
2022-02-08 18:42:04 -0500

    Implement settings service

--- src/services/init.go
+++ src/services/init.go
@@ -2,4 +2,5 @@ package services
 
 func init() {
 	ensureNotebooksDirExists()
+	ensureSettingsFileExists()
 }
---
+++ src/services/settings.go
@@ -0,0 +1,134 @@
+package services
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+
+	util "eno/src/util"
+)
+
+const settingsFile = "build/settings.json"
+
+// ensureSettingsFileExists will create the settings file if it does not already exist.
+func ensureSettingsFileExists() {
+	if _, err := os.Stat(settingsFile); errors.Is(err, fs.ErrNotExist) {
+		err := os.WriteFile(settingsFile, []byte("{}"), 0755)
+		util.CheckError(err)
+	}
+}
+
+// readSettings reads the settings file into memory.
+func readSettings() (map[string]interface{}, error) {
+	settingsJson, err := os.ReadFile(settingsFile)
+	if err != nil {
+		log.Printf("Error occurred reading settings file (%s): %s", settingsFile, err)
+		return nil, fmt.Errorf("an unexpected error occurred while getting app settings, check the logs for more details")
+	}
+
+	settings := make(map[string]interface{})
+	err = json.Unmarshal(settingsJson, &settings)
+	if err != nil {
+		log.Printf("Error occurred parsing settings file JSON (%s): %s", settingsFile, err)
+		return nil, fmt.Errorf("an unexpected error occurred while getting app settings, check the logs for more details")
+	}
+
+	return settings, nil
+}
+
+// writeSettings writes the settings to the settings file.
+func writeSettings(settings map[string]interface{}) error {
+	settingsJson, err := json.Marshal(settings)
+	if err != nil {
+		log.Printf("Error occurred stringifying settings file JSON (%s): %s", settingsFile, err)
+		return fmt.Errorf("an unexpected error occurred while saving app settings, check the logs for more details")
+	}
+
+	err = os.WriteFile(settingsFile, settingsJson, 0755)
+	if err != nil {
+		log.Printf("Error occurred writing settings file (%s): %s", settingsFile, err)
+		return fmt.Errorf("an unexpected error occurred while saving app settings, check the logs for more details")
+	}
+
+	return nil
+}
+
+/*
+GetSettings gets all settings.
+
+	returns: the settings, or an error.
+*/
+func GetSettings() (map[string]interface{}, error) {
+	settings, err := readSettings()
+	if err != nil {
+		return nil, err
+	}
+
+	return settings, nil
+}
+
+/*
+GetSettingsOption gets an option from the settings.
+
+	key:     the name of the option to get from settings.
+
+	returns: the option from settings, or an error.
+*/
+func GetSettingsOption(key string) (interface{}, error) {
+	settings, err := readSettings()
+	if err != nil {
+		return nil, err
+	}
+
+	return settings[key], nil
+}
+
+/*
+SetSettingsOption sets an option in the settings.
+
+	key:     the name of the option.
+	value:   the value of the option.
+
+	returns: an error, if one occurs.
+*/
+func SetSettingsOption(key string, value interface{}) error {
+	settings, err := readSettings()
+	if err != nil {
+		return err
+	}
+
+	settings[key] = value
+
+	err = writeSettings(settings)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+DeleteSettingsOption deletes an option from the settings.
+
+	key:     the name of the option.
+
+	returns: an error, if one occurs.
+*/
+func DeleteSettingsOption(key string) error {
+	settings, err := readSettings()
+	if err != nil {
+		return err
+	}
+
+	delete(settings, key)
+
+	err = writeSettings(settings)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
