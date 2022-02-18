ca117779130bd90f1314380e64469f1873196acc
WKHAllen <wkhallen@gmail.com>
2022-02-17 19:37:12 -0500

    Define color palette

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,8 +22,10 @@ import { WindowService } from './services/window/window.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MatToolbarModule,
   ],
   providers: [
     APIService,
