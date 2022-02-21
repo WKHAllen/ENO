0f793053c0d2034c68dd939293b881ec7650c18d
WKHAllen <wkhallen@gmail.com>
2022-02-21 15:00:19 -0500

    Switch to indigo color scheme

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 import { APIService } from './services/api/api.service';
 import { NotebookService } from './services/notebook/notebook.service';
@@ -18,13 +20,19 @@ import { SettingsService } from './services/settings/settings.service';
 import { WindowService } from './services/window/window.service';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SpinnerComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MatProgressSpinnerModule,
     MatToolbarModule,
   ],
   providers: [
---
+++ webui/src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * A loading spinner.
+ */
+@Component({
+  selector: 'eno-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+})
+export class SpinnerComponent {}
